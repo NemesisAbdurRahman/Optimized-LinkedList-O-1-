e8e8896a1caa5e113394ecb69547a1aaad97191e
NemesisAbdurRahman <abdurrahmannamesis@gmail.com>
2025-05-07 19:42:07 +0600

    pop back and front back

--- doubly_link_list.cpp
+++ doubly_link_list.cpp
@@ -1,83 +1,108 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
+class Node
+{
 public:
-int val;
-Node* next;
-Node* prev;
-Node(int val){
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
 
-this->val=val;
-next=NULL;
-prev=NULL;
-}
-};
-class ll{
-    public:
-int value;
-Node* head=NULL;
-Node* trv=NULL;
-Node* front=head;
-void push_back(int val){
-if(head==NULL){
-value=val;
-Node* newnode=new Node(val);
-head=newnode;
-head->prev=NULL;
-trv=head;
-return ;
-}
-else{
-Node* newnode=new Node(val);
-// trv=head;
-Node* add=trv;
-trv->next=newnode;
-trv=newnode;
-trv->prev=add;
-}
-}
-void push_front(int val){
-
-Node* newnode=new Node(val);
-if(head!=NULL){
-head->prev=newnode;
-// front=newnode;
-newnode->next=head;
-head=newnode;
-// trv=head;
-
-}else{
-    head=newnode;
-trv=head;
-
-}
-}
-
-void print(void){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->val<<endl;
-        temp=temp->next;
+        this->val = val;
+        next = NULL;
+        prev = NULL;
     }
-}
-
-
-
 };
-int main(){
-    
+class ll
+{
+public:
+    int value;
+    Node *head = NULL;
+    Node *trv = NULL;
+    Node *front = head;
+    void push_back(int val)
+    {
+        if (head == NULL)
+        {
+            value = val;
+            Node *newnode = new Node(val);
+            head = newnode;
+            head->prev = NULL;
+            trv = head;
+            return;
+        }
+        else
+        {
+            Node *newnode = new Node(val);
+            // trv=head;
+            Node *add = trv;
+            trv->next = newnode;
+            trv = newnode;
+            trv->prev = add;
+        }
+    }
+    void push_front(int val)
+    {
 
+        Node *newnode = new Node(val);
+        if (head != NULL)
+        {
+            head->prev = newnode;
+            // front=newnode;
+            newnode->next = head;
+            head = newnode;
+            // trv=head;
+        }
+        else
+        {
+            head = newnode;
+            trv = head;
+        }
+    }
+    void pop_front(void){
+        if(head!=NULL){
+Node* remove=head;
+head=head->next;
+head->prev=NULL;
+delete remove;
+        }
 
-ll obj;
-// obj.push_back(1);
+    }
 
-obj.push_front(0);
-obj.push_front(-1);
-obj.push_back(2);
-obj.push_back(3);
-obj.push_front(-2);
-obj.print();
+    void pop_back(void){
 
+        Node* remove2=trv;
+        trv=trv->prev;
+        trv->next=NULL;
+        delete remove2;
+    }
 
+    void print(void)
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val <<"<=>";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+};
+int main()
+{
+
+    ll obj;
+    // obj.push_back(1);
+
+    obj.push_front(0);
+    obj.push_front(-1);
+    obj.push_back(2);
+    obj.push_back(3);
+    obj.push_front(-2);
+    obj.push_back(4);
+obj.pop_front();
+obj.pop_back();
+    obj.print();
 }
